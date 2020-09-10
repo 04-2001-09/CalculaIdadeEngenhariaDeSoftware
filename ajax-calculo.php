26b70bcf29c5a9cbc1dc9cfd2840cd936cc1d129
Allan <allan.milani582@gmail.com>
2020-09-09 21:40:44 -0300

    Finalização da primeira versão do projeto

--- ajax-calculo.php
+++ ajax-calculo.php
@@ -1,2 +1,8 @@
 <?php
 header("Content-Type: application/json");
+
+$dataNascimento = $_POST['idade'];
+$nome = $_POST['nome'];
+$date = new DateTime($dataNascimento);
+$interval = $date->diff(new DateTime(date('Y/m/d')));
+echo $interval->format("{$nome}, sua idade é %Y anos");
--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
     $('#idade').mask('00/00/0000');
     $('.calcular').on('click', function(event) {
         event.preventDefault();
+        $('.resposta').empty().hide(800);
         var form = $('#frmCalculaIdade');
         var dados = form.serialize();
         $.ajax({
@@ -10,7 +11,7 @@ jQuery(document).ready(function($) {
             data: dados,
             dataType: "html",
         }).always(function(response) {
-
+            $('.resposta').empty().append(response).show(800);
         });
     });
 });
